ff2673ca7e547ce0c533153e1565d061226337ac
M1zyuk1 <me@mizune.ms>
2026-04-15 13:07:15 +0900

    chore: update mosaic formula to v0.2.1

--- Formula/mosaic.rb
+++ Formula/mosaic.rb
@@ -1,28 +1,28 @@
 class Mosaic < Formula
   desc "TUI command center for running 10+ AI coding agents in parallel"
   homepage "https://github.com/Mizune/mosaic"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Mizune/mosaic/releases/download/v0.2.0/mosaic_0.2.0_darwin_arm64.tar.gz"
-      sha256 "c2ec213b35dfac33d6311a881d45d04e74e090049d7fe3dfb38e6f315c0256f7"
+      url "https://github.com/Mizune/mosaic/releases/download/v0.2.1/mosaic_0.2.1_darwin_arm64.tar.gz"
+      sha256 "8a4fbeee30edb8df5a9a7ab43df3815f2d0412e58e9ec4957133a3a21a1ede05"
     end
     on_intel do
-      url "https://github.com/Mizune/mosaic/releases/download/v0.2.0/mosaic_0.2.0_darwin_amd64.tar.gz"
-      sha256 "6c5ec6398a345a0045d3f34242da046711ab14a7603430feafac791a59e395f5"
+      url "https://github.com/Mizune/mosaic/releases/download/v0.2.1/mosaic_0.2.1_darwin_amd64.tar.gz"
+      sha256 "55e0e9eb6734cf17ed5dcc353353599ac2b18baa89ec9ebfb7a3e53914018695"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Mizune/mosaic/releases/download/v0.2.0/mosaic_0.2.0_linux_arm64.tar.gz"
-      sha256 "1b1272f3baae0690a9c802b5748c5e4bf3b25acef73d7487ef030109565297aa"
+      url "https://github.com/Mizune/mosaic/releases/download/v0.2.1/mosaic_0.2.1_linux_arm64.tar.gz"
+      sha256 "eaafa1b0c81d0c32507f5e1d4923c0327b759136f2af80fb44793def081e4cd6"
     end
     on_intel do
-      url "https://github.com/Mizune/mosaic/releases/download/v0.2.0/mosaic_0.2.0_linux_amd64.tar.gz"
-      sha256 "324dc88058f93207b5486242ba87d95db72080cf477862edf5f5d8456d539b87"
+      url "https://github.com/Mizune/mosaic/releases/download/v0.2.1/mosaic_0.2.1_linux_amd64.tar.gz"
+      sha256 "985783cba8f94e8a0cacc5eed0f16bb2793ae5523ead5220795068d921d132ea"
     end
   end
 
